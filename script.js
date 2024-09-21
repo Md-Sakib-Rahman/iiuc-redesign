b063c63d19175d78d3ec54082c9073dbcddbd20e
Md-Sakib-Rahman <sakibrahman.user@gmail.com>
2024-09-21 16:23:01 +0600

    made query section

--- script.js
+++ script.js
@@ -54,4 +54,20 @@ valueDisplays.forEach((element)=>{
     }
   }, duration);
 })
-  
+
+
+// drop down js
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
